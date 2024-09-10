5f6c273bae72216c52e358c9363c82b16790d201
Muttaqin Mashrafi <mashrafib43@gmail.com>
2024-09-10 18:39:41 +0600

    updated and bugs solved

--- day2-currencyConverter/app.js
+++ day2-currencyConverter/app.js
@@ -20,7 +20,17 @@ input1.addEventListener("focus", () => {
   inputNotOnFocus = input2;
 });
 
+input1.addEventListener("click", () => {
+  inputOnFocus = input1;
+  inputNotOnFocus = input2;
+});
+
 input2.addEventListener("focus", () => {
+  inputOnFocus = input2;
+  inputNotOnFocus = input1;
+});
+
+input2.addEventListener("click", () => {
   inputOnFocus = input2;
   inputNotOnFocus = input1;
 });
@@ -29,18 +39,27 @@ input1.addEventListener("input", (e) => {
   if (/\D/gim.test(e.target.value)) {
     e.target.value = e.target.value.slice(0, e.target.value.length - 1);
   }
-  if(input1.value) {
-    input2.value = calculateExchange(input1.value,input2.value,input1.placeholder,input2.placeholder)
+  if (input1.value) {
+    input2.value = calculateExchange(
+      input1.value,
+      input2.value,
+      input1.placeholder,
+      input2.placeholder
+    );
   }
-
 });
 
 input2.addEventListener("input", (e) => {
   if (/\D/gim.test(e.target.value)) {
     e.target.value = e.target.value.slice(0, e.target.value.length - 1);
   }
-  if(input2.value) {
-    input1.value = calculateExchange(input2.value,input1.value,input2.placeholder,input1.placeholder)
+  if (input2.value) {
+    input1.value = calculateExchange(
+      input2.value,
+      input1.value,
+      input2.placeholder,
+      input1.placeholder
+    );
   }
 });
 
@@ -80,6 +99,12 @@ search.addEventListener("input", (e) => {
             document.querySelector(`#${y}`).querySelector(".currSpan").innerText
           ] / exchanges[country.querySelector(".currSpan").innerText];
         inputOnFocus.placeholder = "1";
+        calculateExchange(
+          Number(inputOnFocus.value),
+          Number(inputNotOnFocus.value),
+          Number(inputOnFocus.placeholder),
+          Number(inputNotOnFocus.placeholder)
+        );
         modal.style.scale = "0";
       });
     });
@@ -89,54 +114,104 @@ search.addEventListener("input", (e) => {
 numberBtns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     e.preventDefault();
-    if (!operation) {
+    if (prevValue) {
+      nextValue += e.target.innerText;
       inputOnFocus.value += e.target.innerText;
-    } else if (operation === "mul") {
+      console.log(
+        "prev",
+        prevValue,
+        "next",
+        nextValue,
+        "inputValue",
+        inputOnFocus.value
+      );
+    } else {
       inputOnFocus.value += e.target.innerText;
-      inputOnFocus.value =
-        Number(inputOnFocus.value.slice(0, inputOnFocus.value.length - 1)) *
-        Number(inputOnFocus.value.slice(-1));
-      operation = null;
-    } else if (operation === "divide") {
-      inputOnFocus.value += e.target.innerText;
-      inputOnFocus.value =
-        Number(inputOnFocus.value.slice(0, inputOnFocus.value.length - 1)) /
-        Number(inputOnFocus.value.slice(-1));
-      operation = null;
-    } else if (operation === "add") {
-      inputOnFocus.value += e.target.innerText;
-      inputOnFocus.value =
-        Number(inputOnFocus.value.slice(0, inputOnFocus.value.length - 1)) +
-        Number(inputOnFocus.value.slice(-1));
-      operation = null;
-    } else if (operation === "subtract") {
-      inputOnFocus.value += e.target.innerText;
-      inputOnFocus.value =
-        Number(inputOnFocus.value.slice(0, inputOnFocus.value.length - 1)) -
-        Number(inputOnFocus.value.slice(-1));
-      operation = null;
-    } else if (operation === "add") {
-      inputOnFocus.value += e.target.innerText;
-      inputOnFocus.value =
-        Number(inputOnFocus.value.slice(0, inputOnFocus.value.length - 1)) +
-        Number(inputOnFocus.value.slice(-1));
-      operation = null;
+      calculateExchange(
+        Number(inputOnFocus.value),
+        Number(inputNotOnFocus.value),
+        Number(inputOnFocus.placeholder),
+        Number(inputNotOnFocus.placeholder)
+      );
     }
 
-    inputNotOnFocus.value = calculateExchange(inputOnFocus.value);
+    if (operationEQ === "equal") {
+      calculate(operation);
+      operationEQ = null;
+    }
   });
 });
 
+function calculate(operation) {
+  if (operation === "mul") {
+    inputOnFocus.value = Number(prevValue) * Number(nextValue);
+    operation = null;
+    prevValue = "";
+    nextValue = "";
+    calculateExchange(
+      Number(inputOnFocus.value),
+      Number(inputNotOnFocus.value),
+      Number(inputOnFocus.placeholder),
+      Number(inputNotOnFocus.placeholder)
+    );
+  } else if (operation === "divide") {
+    inputOnFocus.value = Number(prevValue) / Number(nextValue);
+    operation = null;
+    prevValue = "";
+    nextValue = "";
+    calculateExchange(
+      Number(inputOnFocus.value),
+      Number(inputNotOnFocus.value),
+      Number(inputOnFocus.placeholder),
+      Number(inputNotOnFocus.placeholder)
+    );
+  } else if (operation === "add") {
+    inputOnFocus.value = Number(prevValue) + Number(nextValue);
+    operation = null;
+    prevValue = "";
+    nextValue = "";
+    calculateExchange(
+      Number(inputOnFocus.value),
+      Number(inputNotOnFocus.value),
+      Number(inputOnFocus.placeholder),
+      Number(inputNotOnFocus.placeholder)
+    );
+  } else if (operation === "subtract") {
+    inputOnFocus.value = Number(prevValue) - Number(nextValue);
+    operation = null;
+    prevValue = "";
+    nextValue = "";
+    calculateExchange(
+      Number(inputOnFocus.value),
+      Number(inputNotOnFocus.value),
+      Number(inputOnFocus.placeholder),
+      Number(inputNotOnFocus.placeholder)
+    );
+  }
+}
+
+let operationEQ = null;
 let operation = null;
+let prevValue = "";
+let nextValue = "";
 
 operatorBtns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     e.preventDefault();
     const operatorSelected = e.target.dataset.operation;
-    console.log(operatorSelected);
     switch (operatorSelected) {
       case "ac":
-        inputOnFocus.value = "";
+        inputOnFocus.value = "0";
+        calculateExchange(
+          Number(inputOnFocus.value),
+          Number(inputNotOnFocus.value),
+          Number(inputOnFocus.placeholder),
+          Number(inputNotOnFocus.placeholder)
+        );
+        operation = null;
+        prevValue = "";
+        nextValue = "";
+        operationEQ = null;
         break;
 
       case "delete":
@@ -144,34 +219,65 @@ operatorBtns.forEach((btn) => {
           0,
           inputOnFocus.value.length - 1
         );
+
+        operation = null;
+        prevValue = "";
+        nextValue = "";
+        operationEQ = null;
+        calculateExchange(
+          Number(inputOnFocus.value),
+          Number(inputNotOnFocus.value),
+          Number(inputOnFocus.placeholder),
+          Number(inputNotOnFocus.placeholder)
+        );
         break;
 
       case "mul":
+        prevValue = inputOnFocus.value;
         operation = "mul";
+        inputOnFocus.value = "";
         break;
 
       case "divide":
+        prevValue = inputOnFocus.value;
         operation = "divide";
+        inputOnFocus.value = "";
         break;
 
       case "add":
+        prevValue = inputOnFocus.value;
         operation = "add";
+        inputOnFocus.value = "";
         break;
 
       case "subtract":
+        prevValue = inputOnFocus.value;
         operation = "subtract";
+        inputOnFocus.value = "";
+        break;
+
+      case "equal":
+        calculate(operation);
+        operation = null;
+        prevValue = "";
+        nextValue = "";
+        operationEQ = null;
+
         break;
     }
   });
 });
 
-function calculateExchange(numberOnfoucus, numberNotOnFocus,inputOnFocusPlaceholder,inputNotOnFocusPlaceholder) {
-  if(!inputOnFocus.value) return 
-  return inputNotOnFocus.value = (
-    numberOnfoucus * inputNotOnFocusPlaceholder / inputOnFocusPlaceholder
-  )
+function calculateExchange(
+  numberOnfoucus = 0,
+  numberNotOnFocus = 0,
+  inputOnFocusPlaceholder,
+  inputNotOnFocusPlaceholder
+) {
+  if (!inputOnFocus.value) return;
+  return (inputNotOnFocus.value =
+    (numberOnfoucus * inputNotOnFocusPlaceholder) / inputOnFocusPlaceholder);
 }
-
 
 openModalBtns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
@@ -208,10 +314,14 @@ openModalBtns.forEach((btn) => {
             document.querySelector(`#${y}`).querySelector(".currSpan").innerText
           ] / exchanges[country.querySelector(".currSpan").innerText];
         inputOnFocus.placeholder = "1";
-        calculateExchange(inputOnFocus.value, inputNotOnFocus.value,inputOnFocus.placeholder,inputNotOnFocus.placeholder);
+        calculateExchange(
+          Number(inputOnFocus.value),
+          Number(inputNotOnFocus.value),
+          Number(inputOnFocus.placeholder),
+          Number(inputNotOnFocus.placeholder)
+        );
         modal.style.scale = "0";
       });
     });
   });
 });
-
